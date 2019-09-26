e38d38ae70a5f2d03f28e45354cde4ddf09d417d
Divya <enjamuridivya@gmail.com>
2019-09-26 12:06:43 -0700

    heroku schema file added

---
+++ db/heroku_schema.sql
@@ -0,0 +1,8 @@
+USE quzvka8z2pkzcmov;
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
